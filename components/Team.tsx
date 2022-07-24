1bff9d777e5452d7388de0ab7b143aef5890fe73
Deployer6 <yjmin83@naver.com>
2022-07-24 12:55:34 +0900

    first commit My PC

--- components/Team.tsx
+++ components/Team.tsx
@@ -6,28 +6,28 @@ import TeamCard from "./TeamCard";
 // @ name: 팀원 이름, position: 포지션, image: public/images에 있는 이미지 이름, color: color 이름 (모든 색상이 가능하지는 않습니다.)
 const teamCardConfig = [
   {
-    name: "h662",
+    name: "정희진",
     position: "Product Manager",
     image: "team1.png",
     color: "blue",
     animation: "tada",
   },
   {
-    name: "h663",
+    name: "이태석",
     position: "Frontend",
     image: "team2.png",
     color: "yellow",
     animation: "tada",
   },
   {
-    name: "h664",
+    name: "육정민",
     position: "Graphic Design",
     image: "team3.png",
     color: "green",
     animation: "tada",
   },
   {
-    name: "h665",
+    name: "윤진재",
     position: "Smart Contract",
     image: "team4.png",
     color: "red",
