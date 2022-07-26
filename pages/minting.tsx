07ed76685ec67094aca68b0689ef97a14409962d
Deployer6 <yjmin83@naver.com>
2022-07-27 00:32:51 +0900

    UFBC test 02

--- pages/minting.tsx
+++ pages/minting.tsx
@@ -40,7 +40,7 @@ const Minting: NextPage = () => {
         type: "SMART_CONTRACT_EXECUTION",
         from: account,
         to: MINT_NFT_ADDRESS,
-        value: caver?.utils.convertToPeb(20, "KLAY"),
+        //value: caver?.utils.convertToPeb(20, "KLAY"),
         gas: 3000000,
         data: mintNFTContract?.methods.mintNFT().encodeABI(),
       });
@@ -64,7 +64,7 @@ const Minting: NextPage = () => {
               const imageResponse = await axios.get(tokenURI);
 
               if (imageResponse.status === 200) {
-                //setNewNFT(imageResponse.data);
+                setNewNFT(imageResponse.data);
               }
             }
           }
